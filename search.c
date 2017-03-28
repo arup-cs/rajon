11cb33d2953bd217488ab15d7966955c08af1f1c
arup-cs <arup.apee@gmail.com>
2017-03-28 02:05:23 -0500

    Binary skeleton

--- search.c
+++ search.c
@@ -12,6 +12,33 @@ int searchLinear (int s, int *list, int n){
 	
 }
 
+int searchBinary(int s, int * list, int n){
+	int i,middle,right;
+	static int left=0;
+	right=n-1;
+	middle=(left+right)/2;
+	if(left>=right){
+		return -1;
+	}
+	if(s==list[middle]){
+		return middle;
+	}else if(s>list[middle]){
+		left=middle;
+	}else if(s<list[middle]){
+		right=middle;
+	}
+	searchBinary(s, list, right);
+	
+}
+
+void printResult(int userInput, int result, char* string){
+		if(result>0){
+		printf("Using %s Search : The value %d occurs at position %d\n",string, userInput,result);
+	}else{
+		printf("Returned value=-1: The number does not occur in the array\n");
+	}
+}
+
 int main(){
 	int i,numbers[10], userInput,result;
 	
@@ -26,11 +53,9 @@ int main(){
 	printf("What value you are searching?");
 	scanf("%d",&userInput);
 	result=searchLinear(userInput, numbers,10);
-	if(result>0){
-		printf("Using Linear Search : The value %d occurs at position %d\n",userInput,result);
-	}else{
-		printf("Returned value=-1: The number does not occur in the array\n");
-	}
-
+	printResult(userInput, result, "Linear");
+	result=searchBinary(userInput, numbers,10);
+	printResult(userInput, result, "Binary");
 	return 0;
+
 	}
