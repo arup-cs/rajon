05f7a7f9c67cba84db3d768b910f4da90ab701e3
arup-cs <arup.apee@gmail.com>
2017-03-28 01:34:16 -0500

    Linear Ok

--- search.c
+++ search.c
@@ -1,9 +1,19 @@
 #include<stdio.h>
 #include<stdlib.h>
 
+int searchLinear (int s, int *list, int n){
+	int i;
+	for (i=0;i<n;i++){
+		if(s==list[i]){
+			return i;
+		}
+	}
+	return -1;
+	
+}
 
 int main(){
-	int i,numbers[10], userInput;
+	int i,numbers[10], userInput,result;
 	
 	for(i=0;i<10;i++){
 		printf("Enter a number:");
@@ -15,8 +25,12 @@ int main(){
 	}
 	printf("What value you are searching?");
 	scanf("%d",&userInput);
-	
-	
+	result=searchLinear(userInput, numbers,10);
+	if(result>0){
+		printf("Using Linear Search : The value %d occurs at position %d\n",userInput,result);
+	}else{
+		printf("Returned value=-1: The number does not occur in the array\n");
+	}
 
 	return 0;
 	}
